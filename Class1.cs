7409c3ebba262ca63d7475b9c739238b6723e860
MILES FELICIANO <115053837+SHIKORI@users.noreply.github.com>
2022-10-19 13:38:37 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brrt_brrt_2
+{
+    class Person
+    {
+        public string name { get; set; }
+        public int BirthYear { get; set; }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace brrt_brrt_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.name = "Angel" + "Locsin";
+            person1.BirthYear = 30;
+            Console.WriteLine("Hello " + person1.name);
+            Console.WriteLine("You are " + person1.BirthYear + " years old.");
+        }
+    }
+}
